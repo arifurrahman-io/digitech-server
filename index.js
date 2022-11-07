cf82dd1ce8453873eccdb4d94a9c0ff26d310ce2
arifurrahman01 <arifurrahman.now@gmail.com>
2022-11-08 01:07:24 +0600

    database configured

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = process.env.PORT || 5000;
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@friitraining.a5d8fvh.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const serviceCollection = client.db('wildLife').collection('services');
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query).limit(3);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+    } finally {
+
+    }
+}
+run().catch(err => console.error(err))
+
+app.get('/', (req, res) => {
+    res.send('WildLife server is running')
+})
+
+app.listen(port, () => {
+    console.log(`WildLife server is running on ${port}`)
+})
