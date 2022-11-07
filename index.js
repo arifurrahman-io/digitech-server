b30feaca4a140ad215826541929518b0e8f4f906
arifurrahman01 <arifurrahman.now@gmail.com>
2022-11-08 02:12:57 +0600

    service data sending method added

--- index.js
+++ index.js
@@ -20,7 +20,22 @@ async function run() {
             const cursor = serviceCollection.find(query).limit(3);
             const services = await cursor.toArray();
             res.send(services);
+        });
+
+        app.get('/all-services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const allServices = await cursor.toArray();
+            res.send(allServices);
         })
+
+        app.get('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        });
+
     } finally {
 
     }
